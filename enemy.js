f4ef490129216ddea3ebd7c973505eaf928369b5
Toto Briac <totobriac@gmail.com>
2022-08-07 02:18:03 +0200

    now enmey collision

--- enemy.js
+++ enemy.js
@@ -1,5 +1,6 @@
 import { Sprite } from "./sprite.js";
 import { player, ctx, map } from "./raycasting.js";
+import { normalizeAngle } from "./functions.js";
 
 var enemies = [];
 
@@ -11,18 +12,38 @@ class Enemy extends Sprite {
     super(x, y, image, frame, player, ctx);
     this.level = map;
     this.speed = 1;
-    this.angle = 220;
+    this.angle = 250;
     this.tickCount = 0;
     this.maxTickCount = 12;
     this.isInRange = false;
     this.isShot = false;
     this.life = 5;
+    this.speed = 0.2;
+    this.yFrame = 1;
   }
   draw() {
     this.update();
     super.draw();
   }
+  checkForCollision(x, y) {
+    var collision = false;
+    var xGridNb = Math.floor(x / this.level.mapS);
+    var yGridNb = Math.floor(y / this.level.mapS);
+    if (this.level.checkPlayerCollision(yGridNb, xGridNb)) {
+      collision = true;
+    };
+    return collision;
+  }
   update() {
+
+    var newX = this.x + Math.cos(this.angle * Math.PI /180) * this.speed;
+    var newY = this.y + Math.sin(this.angle * Math.PI /180) * this.speed;
+
+    if (!this.checkForCollision(newX, newY)) {
+      this.x = newX;
+      this.y = newY;
+    }
+
     var X = this.x - this.player.x;
     var Y = this.y - this.player.y;
 
@@ -31,42 +52,51 @@ class Enemy extends Sprite {
     if (p < 0) p += 360;
     if (p > 360) p -= 360;
 
-    var diff = p - this.angle;
+    var diff = (this.player.angle * 180 / Math.PI)  - this.angle;
 
     if (diff < 0) diff += 360;
     if (diff > 360) diff -= 360;
 
     switch (true) {
-      case diff < 25:
-        this.frame = 0
-        break;
-      case diff > 25 && diff < 65:
-        this.frame = 1
-        break;
-      case diff > 65 && diff < 115:
-        this.frame = 2
-        break;
-      case diff > 115 && diff < 155:
-        this.frame = 3
-        break;
-      case diff > 155 && diff < 205:
+      case diff < 22.5:
         this.frame = 4
         break;
-      case diff > 205 && diff < 245:
-        this.frame = 5
+      case diff > 22.5 && diff < 67.5:
+        this.frame = 3
         break;
-      case diff > 245 && diff < 285:
-        this.frame = 6
+      case diff > 67.5 && diff < 112.5:
+        this.frame = 2
         break;
-      case diff > 285 && diff < 325:
+      case diff > 112.5 && diff < 157.5:
+        this.frame = 1
+        break;
+      case diff > 157.5 && diff < 202.5:
+        this.frame = 0
+        break;
+      case diff > 202.5 && diff < 247.5:
         this.frame = 7
         break;
-      case diff > 325:
-        this.frame = 0
+      case diff > 247.5 && diff < 292.5:
+        this.frame = 6
+        break;
+      case diff > 292.5 && diff < 337.5:
+        this.frame = 5
+        break;
+      case diff > 337.5:
+        this.frame = 4
         break;
       }
 
     this.imageX = this.frame * 64;
+
+    if (this.tickCount > this.maxTickCount) {
+      this.yFrame < 4 ? this.yFrame ++ : this.yFrame = 1;
+      this.tickCount = 0;
+    } else {
+      this.tickCount ++;
+    }
+
+    this.imageY = this.yFrame * 64;
   }
 }
 
